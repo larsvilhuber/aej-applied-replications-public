dfbf607d4a61665d9ec83deb8e3c6581be304645
Lars Vilhuber <lars@vilhuber.com>
2024-02-29 11:14:30 -0500

    Updating default docker image to latest

---
+++ run_docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ "$1" == "-h" ]]
+then
+cat << EOF
+$0 (tag)
+
+will start interactive shell for tag (TAG)
+EOF
+exit 0
+fi
+
+PWD=$(pwd)
+. config.sh
+tag=${1:-$tag}
+
+case $USER in
+  *vilhuber|*herbert)
+  WORKSPACE=$PWD
+  ;;
+  codespace)
+  WORKSPACE=/workspaces
+  ;;
+esac
+  
+# build the docker if necessary
+
+docker pull $space/$repo:$tag
+
+OPTIONS="-it --rm --entrypoint /bin/bash -w /home/rstudio"
+# OPTIONS="-e DISABLE_AUTH=true  --rm -p 8787:8787"
+
+docker run -v "$WORKSPACE":/home/rstudio $OPTIONS $space/$repo:$tag
